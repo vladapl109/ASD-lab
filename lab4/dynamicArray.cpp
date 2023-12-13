b867860f36473b04995f013a7f8e1a4f32457626
vladapl109 <145658252+vladapl109@users.noreply.github.com>
2023-12-13 20:53:43 +0200

    Update dynamicArray.cpp

--- lab4/dynamicArray.cpp
+++ lab4/dynamicArray.cpp
@@ -1,12 +1,12 @@
 #include "dynamicArray.h"
  
-    DynamicArray::DynamicArray(int capacity){
-        this->values = new int[capacity];
-        this->capacity = capacity;
+    DynamicArray::DynamicArray(int capacity){ //объявление конструктора для класса DynamicArray
+        this->values = new int[capacity]; //Выделение динамической памяти для массива values размером capacity
+        this->capacity = capacity; //Значение переменной capacity, переданное в конструктор, присваивается переменной capacity класса
     }
 
-    void DynamicArray::coutValues(){
-        for(int i = 0; i<this->length; i++){
+    void DynamicArray::coutValues(){ //Объявление метода coutValues класса DynamicArray. void указывает на то, что метод не возвращает значения
+        for(int i = 0; i<this->length; i++){ //проходит по всем элементам массива values
             std::cout<<this->values[i]<<" ";
         }
         std::cout<<std::endl<<this->capacity<<std::endl;
